3e468d0ffe08171d43e2ae5ff072721443965a6e
Shriphani Palakodety <shriphanip@gmail.com>
2014-06-17 12:51:34 -0400

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject subotai "0.2.0"
+(defproject subotai "0.2.1"
   :description "Mining HTML documents"
   :url "https://github.com/shriphani/subotai"
   :license {:name "Eclipse Public License"
