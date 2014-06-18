076ff9c66237279ecef753f5962a0426534a9520
Shriphani Palakodety <shriphanip@gmail.com>
2014-06-17 21:07:41 -0400

    fixed shingle creation bug

--- src/subotai/near_duplicate/shingles.clj
+++ src/subotai/near_duplicate/shingles.clj
@@ -25,15 +25,22 @@
           (-> ptb-tok .next .toString)
           nil))))))
 
+(defn text-n-grams
+  [text]
+  (let [toks-seq-fn (fn [toks]
+                      (map
+                       (fn [n]
+                         (drop n toks))
+                       (range size-of-grams)))]
+    (set
+     (apply map vector (-> text
+                           doc->tokens-seq
+                           toks-seq-fn)))))
+
 (defn near-duplicate?
   [text1 text2]
-  (let [toks1 (doc->tokens-seq text1)
-        toks2 (doc->tokens-seq text2)
-
-        shingles1 (set
-                   (partition size-of-grams toks1))
-        shingles2 (set
-                   (partition size-of-grams toks2))
+  (let [shingles1 (text-n-grams text1)
+        shingles2 (text-n-grams text2)
 
         common (clojure.set/intersection shingles1
                                          shingles2)
