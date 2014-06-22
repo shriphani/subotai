b74946b68f87b96d6d0282cb984c5679984a223f
Shriphani Palakodety <shriphanip@gmail.com>
2014-06-22 08:25:55 +0800

    added fingerprinting code

--- src/subotai/near_duplicate/fingerprint.clj
+++ src/subotai/near_duplicate/fingerprint.clj
@@ -1,35 +1,63 @@
 (ns subotai.near-duplicate.fingerprint
   "A fingerprinting based near-duplicate detection
    algorithm from Charikar et al"
-  (:require [clojure.string :as string]
-            [subotai.near-duplicate.utils :refer :all])
-  (:import [edu.stanford.nlp.process
-            CoreLabelTokenFactory
-            DocumentPreprocessor
-            Morphology
-            PTBTokenizer]
-           [java.io StringReader]))
+  (:require [digest]
+            [subotai.near-duplicate.utils :refer :all]))
 
-(defn doc->features
+(def num-dimensions 64)
+
+(def set-bits-nums (map
+                    (fn [i] (bit-set 0 i))
+                    (range num-dimensions)))
+
+(defn doc->vector
   [a-document]
-  (let [morph  (Morphology.)
-        tokens (map
-                (fn [t]
-                  (.stem morph t))
-                (map
-                 string/lower-case
-                 (filter
-                  (fn [t]
-                    (not
-                     (re-find #"\p{Punct}" t)))
-                  (doc->tokens-seq a-document))))]
+  (let [toks (doc->cleaned-non-stopwords-tokens-seq a-document)]
     (reduce
      (fn [acc t]
-       (merge-with + acc {t 1}))
+       (merge-with +' acc {t 1}))
      {}
-     tokens)))
+     toks)))
 
-;; (defn doc->fingerprint
-;;   [a-document]
-;;   (let [features (doc->features a-document)]
-;;     ))
+(defn vector->fingerprint
+  "If item at position is positive, then 1
+   else 0"
+  [vector]
+  (reduce
+   (fn [x [i v]] 
+     (if (pos? v)
+       (bit-set x i)
+       x))
+   (long 0)
+   (map-indexed (fn [i v] [i v])
+                vector)))
+
+(defn hash-doc-vector
+  [a-document]
+  (let [doc-vector (doc->vector a-document)
+
+        hash-vector (long-array 64)]
+    (doseq [[t w] doc-vector] ; term-freq (weight)
+      (let [hashed-value (-> t
+                             digest/md5
+                             (.getBytes)
+                             bigint
+                             unchecked-long)]
+
+        (do (doseq [[i x] (map-indexed (fn [i x] [i x]) set-bits-nums)]
+              (let [zero-at-pos
+                    (zero? (bit-and hashed-value x))]
+                (if zero-at-pos
+                  (aset hash-vector i (-
+                                       (aget hash-vector i)
+                                       w)) ; decrement by weight
+                  (aset hash-vector i (+
+                                       (aget hash-vector i)
+                                       w)))))))) ; increment by weight
+    hash-vector))
+
+(defn fingerprint
+  [a-document]
+  (-> a-document
+      hash-doc-vector
+      vector->fingerprint))
--- src/subotai/near_duplicate/utils.clj
+++ src/subotai/near_duplicate/utils.clj
@@ -1,10 +1,20 @@
 (ns subotai.near-duplicate.utils
   "Common utils for the near-duplicate codebase"
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string])
   (:import [edu.stanford.nlp.process
             CoreLabelTokenFactory
             DocumentPreprocessor
+            Morphology
             PTBTokenizer]
            [java.io StringReader]))
+
+(def stoplist-file "stoplist.dft")
+
+(def stoplist (set
+               (string/split-lines
+                (slurp
+                 (io/resource stoplist-file)))))
 
 (defn doc->tokens-seq
   "Converts a doc to a sequence
@@ -20,3 +30,31 @@
         (if (.hasNext ptb-tok)
           (-> ptb-tok .next .toString)
           nil))))))
+
+(defn doc->cleaned-tokens-seq
+  "Converts a doc to a sequence of
+   case-folded, punctuation-removed,
+   stemmed tokens"
+  [text]
+  (let [toks-seq (doc->tokens-seq text)
+
+        morph (Morphology.)]
+    (map
+     (fn [t]
+       (->> t
+            string/lower-case
+            (.stem morph)))
+     (filter
+      (fn [t]
+        (not
+         (re-find #"\p{Punct}" t)))
+      toks-seq))))
+
+(defn doc->cleaned-non-stopwords-tokens-seq
+  [text]
+  (let [cleaned-tokens-seq (doc->cleaned-tokens-seq text)]
+    (filter
+     (fn [t]
+       (not
+        (some #{t} stoplist)))
+     cleaned-tokens-seq)))
